ebafba6e01a1c9f22a3f47e7740151c8e4e0c540
molly <molly.salzberger@gmail.com>
2023-10-02 17:46:53 -0500

    pseudocode everywhere

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,57 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  
   //prompt (write to page or pop up??) w/criteria choices
-  //get password criteria from choices
+  //get password criteria from choices:
     //prompt choosing password length bwtn 8 & 128
+    //get length function??
+    var thingLength;
+    function getLength() {
+      thingLength = prompt("Choose a password length between 8 and 128");
+      //make it evaluate as int somehow?
+      //if statement to validate input
+      if (thingLength < 8) {
+        alert("Not in range. Please try again.")
+      } else if (thingLength > 128) {
+        alert("Not in range. Please try again.")
+      } else {
+        console.log(thingLength);
+        return thingLength;
+      }
+    }
+    getLength();
     //set length variable
+    
+  
     // ask about this list of character types: lowercase, uppercase, numeric, and/or special characters
-    //store those as variables? requisites/limitations for randomizers
-  //plug those variables into so many randomizers
+    var charTypes;
+    function charOptions(){
+      charTypes = prompt("Choose character types:");
+      //put a list to select from in the prompt
+      //put into array
+      return charTypes;
+    }
+    charOptions();
+   
+
+
+
+    //create array/object of length thingLength
+var finalWord;
+function mixingBowl(){
+    //numbers array
+    //letters array (just a list, use split)
+    //special characters array (list? using split again?)
+//for each until thingLength - 1 (because index starts at 0), from prev defined parameter arrays
+return finalWord;
+};
+  
+
   //return statement? i think
+  return finalWord;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
