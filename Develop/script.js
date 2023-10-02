9b0d863f4ab1aa9812d9df7be6174a1cccd9d49b
molly <molly.salzberger@gmail.com>
2023-10-02 16:56:32 -0500

    notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  //prompt (write to page or pop up??) w/criteria choices
+  //get password criteria from choices
+    //prompt choosing password length bwtn 8 & 128
+    //set length variable
+    // ask about this list of character types: lowercase, uppercase, numeric, and/or special characters
+    //store those as variables? requisites/limitations for randomizers
+  //plug those variables into so many randomizers
+  //return statement? i think
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
