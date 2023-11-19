625059c7bda90551f50d3d0b2a221e0d9b87394b
molly <molly.salzberger@gmail.com>
2023-11-19 14:26:53 -0600

    it produces characters.

--- script.js
+++ script.js
@@ -1,54 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// global arrays = easier?
+var lowAlpha = ["a","b","c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["-", "_", "+", "=", "!", "#", "$", "*", "?", "%"]; // find somewhere
+var someNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+
+
+function getrandomIndex(min, max){
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 function generatePassword(){
-  var allLettersLow= ["abcdefghijklmnopqrstuvwxyz"]; //find split function for this
-  var allLettersUp = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; //again, neet to split
-  var specialCharList;
-  var intList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  //prompt (write to page or pop up??) w/criteria choices
-  //get password criteria from choices:
-    //prompt choosing password length bwtn 8 & 128
-    //get length function??
-    var thingLength;
-    function getLength() {
-      thingLength = prompt("Choose a password length between 8 and 128");
-      //make it evaluate as int somehow?
-      //if statement to validate input
-      if (thingLength < 8) {
-        alert("Not in range. Please try again.")
-      } else if (thingLength > 128) {
-        alert("Not in range. Please try again.")
-      } else {
-        console.log(thingLength);
-        return thingLength;
-      }
-    }
-    getLength();
-    //set length variable
-   
-    var charTypes;
-    function allOptions(){
-      charTypes = prompt("Choose character types:");
-      //maybe var yesNo to use in an if/else deciding whether to add that array to the answer array???
-      // how many prompts can I put in a variable? Do I really have to figure out how to do the modal dialog thing for this?
-      //lowercase y/n. if y, add that array to answer array
-      // uppercase y/n. same
-      // integers y/n. same
-      //special characters y/n. same
-      // charTypes = answers array.
-      return charTypes; //or something? i think i need it to be a variable in a wider/maybe global scope.
-    }
-    allOptions();
-   
-  function mixingBowl(){
-    //add generate array finalWord from array allOptions of length thingLength
-    // for 
-  return finalWord;
-}; 
-  //return statement? i think
-  return finalWord;
+
+
+let allArray = [];
+
+function allOptions(){
+
+  upperCase = window.confirm("Include uppercase letters?");
+  if (upperCase === true){
+    allArray = allArray.concat(upAlpha);
+    console.log("yes uppercase")
+  } else {
+    console.log("no uppercase letters")
+  };
+
+  lowerCase = window.confirm("Include lowercase letters?")
+  if (lowerCase === true){
+    allArray = allArray.concat(lowAlpha);
+    console.log("yes lowercase")
+  } else {
+    console.log("no lowercase letters")
+  };
+
+  moreChars = window.confirm("Include special characters?")
+  if (moreChars === true){
+    allArray = allArray.concat(specialChar);
+    console.log("yes special characters")
+  } else {
+    console.log("no special characters")
+  };
+  // var totalLength = allArray.length();
+  console.log(allArray)
+  return allArray; 
+};
+
+var pickLength;
+function getLength() {
+  pickLength = Number(window.prompt("Choose a password length between 8 and 128", ""));
+  if (pickLength < 8) {
+    alert("Not in range. Please try again.")
+  } else if (pickLength > 128) {
+    alert("Not in range. Please try again.")
+  } else {
+    console.log(pickLength);  
+  }
+};
+
+getLength();
+allOptions();
+
+let finalWord = [];
+var totalLength = allArray.length;
+function mixingBowl(){
+  for (i = 0; i < pickLength; i++){
+    finalWord.push(allArray[Math.floor(Math.random() * totalLength)]);
+  }
+  
+ return finalWord;
 }
+mixingBowl();
+// this join should be removing the commas and it isn't. i don't know why and i'm tired of trying different methods to make this work. NOT worth the time i've wasted on it.
+yourPass = finalWord.join();
+return yourPass;
+  };
+
+
 
 // Write password to the #password input
 function writePassword() {
